3be4ccb7234ec374925a6e74b20c8b07be5bf182
chaerang <apotheosis408@naver.com>
2023-11-13 17:44:01 +0900

    12th

--- 12thweek.py
+++ 12thweek.py
@@ -71,20 +71,153 @@
 #     i += 1
 #     rl(ln, folder + f"{i}.jpg")
     
-# #import pandas.Series
+# # #import pandas.Series
+# from pandas import Series as sr
+# # import numpy as np
+    
+    
+# # data - np.arange(5)
+# data = [10, 20,30, 40]
+# sdata = sr(data)    
+
+# print(sdata)
+
+# # # print(sdata.index)
+# # print(sdata.index.to_list())
+
+
+# sdata.index = ["a", "b", "c", "d"]
+# print(sdata)
+
 from pandas import Series as sr
-# import numpy as np
-    
-    
-# data - np.arange(5)
-data = [10, 20,30, 40]
-sdata = sr(data)    
 
-print(sdata)
+# dt = [10, 20,30, 40]
+# idx = ["a", "b", "c", "d"]
+# # sdata = sr(data)    
 
-# # print(sdata.index)
-# print(sdata.index.to_list())
+# # print(sdata)
+
+# # # sdata.index = ["a", "b", "c", "d"]
+
+# # # sdata = sr(data=idx, index=dt)
+# # # sdata - sr(data=dt, index=idx)
+# # # sdata = sr(idx, dt)
+# # sdata = sr(dt, idx)
+# # print(sdata)
+
+# sdata = sr(dt, idx)
+
+# # sd = sdata.reindex["a", "j", "c"]
+# # sd = sdata.reindex["a", "c"]
+# sd = sdata.reindex["b"]
+# print(sd)
+# print("\n--------------------------\n")
+# print(sdata["b"])
+
+# print(sdata.iloc[0])
+# print(sdata.iloc[2])
+# print(sdata.iloc["a"])
+# print(sdata.iloc["b"])
+
+# # print(sdata[0])
+
+# dt = ["alpha", "beta", "charlie", "delta"]
+# idx = ["ab", "bc", "cd", "de"]
+
+# sdata = sr(dt, idx)
+
+# print(sdata.loc["bc" : "cd"])
+# print("\n------------------\n")
+# print(sdata.loc["bc" : ])
+# print("\n------------------\n")
+# print(sdata.loc[:"bc"])
+# print("\n------------------\n")
+
+# dt = ["사과", "바나나", "수박", "참외"]
+# idx = ["가", "나", "다", "라"]
+
+# sdata = sr(data=dt, index=idx)
+
+# print(sdata.iloc[1:2])
+# print(sdata.iloc[2:])
+# print(sdata.iloc[:2])
 
 
-sdata.index = ["a", "b", "c", "d"]
-print(sdata)
+# dt = ["alpha", "beta", "charlie", "delta"]
+# idx = ["ab", "bc", "cd", "de"]
+
+# sdata = sr(data=dt, index=idx)
+
+# # sdata.loc["cd"] = "echo"
+# # print(sdata)
+# # sdata.iloc[1] = "fox"
+# # print(sdata)
+
+# sdata.drop("bc")
+# print(sdata)
+
+# s1 = sr([100, 200, 300], index=["a", "b", "c"])
+# s2 = sr([100, 200, 300], index=["b", "c", "a"])
+
+# sum_res = s1 + s2
+# print(sum_res)
+# print(sum_res.max())
+# print(sum_res.min())
+# print(sum_res.mean())
+
+# sub_res = s1 - s2
+# print(sub_res)
+# print(sub_res.max())
+# print(sub_res.min())
+# print(sub_res.mean())
+
+# mul_res = s2 * 10
+# print(mul_res)
+
+# div_res = s1 / 10
+# print(div_res)
+
+# data = {
+#     "삼성전자": "전기,전자",
+#     "LG전자": "전기,전자",
+#     "현대차": "운수장비",
+#     "NAVER": "서비스업",
+#     "카카오": "서비스업"
+# }
+
+# sdata = sr(data)
+# uniq = sdata.unique()
+# print(uniq)
+
+# sc = sdata.count()
+# print(sc)
+
+# sv = sdata.value_counts()
+# print(sv)
+
+# idx = ["a", "b", "c", "d", "e"]
+# s1 = sr([1100, 270, 30, 450, 50], index=idx)
+# s2 = sr([150, 740, 810, 40, 25], index=idx)
+
+# # 시리즈내 데이터 비
+# fil = s1 > 300
+# print(fil)
+# print(s1[fil])
+# print(s1[fil].index)
+
+# # 시리즈간 비교
+# fil1 = s2 > s1
+# print(fil1)
+
+# # 인덱싱 출력
+# print(s2[s2 > s1].index)
+
+idx = ["a", "b", "c", "d", "e"]
+dt = [1100, 270, 30, 450, 50]
+s1 = sr(data=dt, index=idx)
+
+sv = s1.sort_values()
+print(sv)
+
+sv1 = s1.sort_values(ascending=False)
+print(sv1)
